95e189825da8ba130fc0f638090831c569f8c3ba
ashenbro134 <129660680+ashenbro134@users.noreply.github.com>
2023-10-31 10:50:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,52 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+global.owner = [
+  ['94726962984', 'AIKILLER', true],
+  ['94726962984'], 
+  ['94726962984'] 
+] //Numeros de owner 
+
+global.mods = ['94726962984'] 
+global.prems = ['94726962984', '94726962984', '94726962984']
+global.APIs = { // API Prefix
+  // name: 'https://website'
+  xteam: 'https://api.xteam.xyz', 
+  nrtm: 'https://fg-nrtm.ddns.net',
+  bg: 'http://bochil.ddns.net',
+  fgmods: 'https://api.fgmods.xyz'
+}
+global.APIKeys = { // APIKey Here
+  // 'https://website': 'apikey'
+  'https://api.xteam.xyz': 'd90a9e986e18778b',
+  'https://zenzapis.xyz': '675e34de8a', 
+  'https://api.fgmods.xyz': 'dEBWvxCY' //--- 100 de límite diario --- Regístrese en https://api.fgmods.xyz/
+}
+
+// Sticker WM
+global.packname = '💝𝗤𝚄𝙴𝙴𝙽𝗔𝙻𝙴𝚇𝙰💝' 
+global.author = '@ashenbro134' 
+global.fgig = '▢ Sígueme en Instagram\nhttps://www.instagram.com/Alexa_ff\n' 
+global.dygp = 'https://chat.whatsapp.com/L341x6tZ2ix11je8QyCs0O'
+global.fgsc = 'https://github.com/ashenbro134/Queen-Alexa-BOT' 
+global.fgyt = 'https://youtube.com/fg98f'
+global.fgpyp = 'https://paypal.me/Alexa'
+global.fglog = '___' 
+
+global.wait = '*⌛ _WAIT..._*\n*▰▰▰▱▱▱▱▱*'
+global.rwait = '⌛'
+global.dmoji = '🤭'
+global.done = '✅'
+global.error = '❌' 
+global.xmoji = '🔥' 
+
+global.multiplier = 69 
+global.maxwarn = '2' // máxima advertencias
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})

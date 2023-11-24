3bb66809684176b0847ff26e915d773fde0e3988
ashenbro134 <129660680+ashenbro134@users.noreply.github.com>
2023-11-24 20:21:08 +0530

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -1,47 +1,46 @@
 
-import fg from 'api-dylux'
-import fetch from 'node-fetch'
-let handler = async (m, { conn, text, args, usedPrefix, command }) => {
-    
-        if (!args[0]) throw `✳️ Ingrese un link de Tiktok\n\n 📌 Ejemplo : ${usedPrefix + command} https://vm.tiktok.com/ZMjkj76X6/`
-        if (!args[0].match(/tiktok/gi)) throw `❎ verifica que el link sea de tiktok`
-        m.react(rwait)
-      try {
-        let res = await fetch(global.API('fgmods', '/api/downloader/tiktok2', { url: args[0] }, 'apikey'))
-        let data = await res.json()
-        
-        if (data.result.video) {
-            let tex = `
-┌─⊷ *TIKTOK DL*
-▢ *Nombre:* ${data.result.author.name}
-▢ *Username:* ${data.result.author.unique_id}
-▢ *Duración:* ${data.result.video.durationFormatted}
-▢ *Calidad:* ${data.result.video.ratio}
-▢ *Likes:* ${data.result.stats.likeCount}
-▢ *Vistas:* ${data.result.stats.playCount}
-▢ *Descripción:* ${data.result.title}
-└───────────
-`
-            conn.sendFile(m.chat, data.result.video.noWatermark, 'tiktok.mp4', tex, m);
-            m.react(done)
-   } else {
-            let cap = `
-▢ *Descripción:* ${data.result.title}
-▢ *Likes:* ${data.result.stats.likeCount}
-`
-            for (let tt of data.result.images) {
-                conn.sendMessage(m.chat, { image: { url: tt.url }, caption: cap }, { quoted: m })
-            }
-            conn.sendFile(m.chat, data.result.music.play_url, 'tiktok.mp3', '', m, null, { mimetype: 'audio/mp4' })
-            m.react(done)
-        }
-    } catch (error) {
-        m.reply(`❎ Error al descargar el video`)
-    }
+import fg from 'api-dylux' 
+import { tiktokdl } from '@bochilteam/scraper'
+let handler = async (m, { conn, text, args, usedPrefix, command}) => {
+if (!args[0]) throw `🌀 please enter the Tiktok Url \n\n ⚡ Example : ${usedPrefix + command} https://vm.tiktok.com`
+if (!args[0].match(/tiktok/gi)) throw `❎ This link has been incorrect`
+m.react('🕗')
+
+try {
+    let p = await fg.tiktok(args[0]) 
+    let te = `
+ 💝𝗤𝗨𝗘𝗘𝗡-𝗔𝗟𝗘𝗫𝗔-𝗧𝗜𝗞𝗧𝗢𝗞-𝗗𝗟💝
+*THE USER NICKNAME*
+🥷 *${p.nickname}*
+*THE USERNAME*
+👻 *${p.unique_id}*
+*THE TIME DURATION* 
+🕗 *${p.duration}*
+*THE VIDEO DESCRIPTION*
+📝 *${p.description}*
+*💝QUEEN_ALEXA_MD💝*`
+    conn.sendFile(m.chat, p.play, 'tiktok.mp4', te, m)
+    m.react(✅)
+    } catch {  	
+    try { 
+	const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw '❎ Error all'
+    conn.sendFile(m.chat, url, 'fb.mp4', `
+💝 𝗔𝗟𝗘𝗫𝗔-𝗧𝗜𝗞𝗧𝗢𝗞2💝
+⦁ *Nickname:* ${nickname} ${description ? `\n⦁ *DESCRIPTION* ${description}` : ''}
+💝𝗤𝗨𝗘𝗘𝗡-𝗔𝗟𝗘𝗫𝗔_𝗠𝗗`, m)
+m.react(done)
+} catch {
+    m.reply(`❎ Error`)
 }
+} 
+    
+}  
 handler.help = ['tiktok']
 handler.tags = ['dl']
-handler.command = ['tiktok', 'tt', 'tiktokimg', 'tiktokslide']
-handler.diamond = true
+handler.command = /^(tiktok|ttdl|tiktokdl|tiktoknowm)$/i
+handler.diamond = false
 
 export default handler
+
